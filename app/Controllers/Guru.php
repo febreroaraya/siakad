57a36498dca22b6cd08ee939c38d2f14565210c0
ahmadfadhiel <55206005+ahmadfadhiel@users.noreply.github.com>
2021-12-27 22:41:09 +0700

    error edit guru

--- app/Controllers/Guru.php
+++ app/Controllers/Guru.php
@@ -88,7 +88,7 @@ class Guru extends BaseController
             //memindahkan file foto dari form input ke folder foto di directory
             $foto->move('fotoguru', $nama_file);
             $this->Mdl_Guru->add($data);
-            session()->setFlashdata('pesan', 'Data berhasil di tambahkan');
+            session()->setFlashdata('pesan', 'Data Berhasil Ditambahkan !!');
             return redirect()->to(base_url('guru'));
         } else {
             //jika tidak valid 
@@ -107,7 +107,7 @@ class Guru extends BaseController
         return view('layout/v_wrapper', $data);
     }
 
-    public function update($id_user)
+    public function update($id_guru)
     {
         if ($this->validate([
             'kode_guru'  => [
@@ -140,28 +140,48 @@ class Guru extends BaseController
             ],
             'foto_guru'  => [
                 'label' => 'Foto Guru',
-                'rules' => 'max_size[foto_guru,1024]|mime_in[foto_guru,image/png,image/jpg,image/jpeg,image/gif,image/ico]',
+                'rules' => 'max_size[foto,1024]|mime_in[foto,image/png,image/jpeg,image/jpg,image/gif,image/ico]',
                 'errors'    =>  [
-                    'max_size'  => '{field} Max 1024 KB',
-                    'mime_in'  => 'Format {field} Wajib PNG, JPG, JPEG, GIF, ICO,'
+                    'max_size'  => '{field} Max 1024 KB!',
+                    'mime_in'  => 'Format {field} Wajib PNG, JPG, JPEG, GIF, ICO!'
                 ]
             ],
         ])) {
             //mengambil file foto dari form input
-            $foto = $this->request->getFile('foto');
-            //merename nama file foto
-            $nama_file = $foto->getRandomName();
-            //jika valid
-            $data = array(
-                'kode_guru' => $this->request->getPost('kode_guru'),
-                'nip' => $this->request->getPost('nip'),
-                'nama_guru' => $this->request->getPost('nama_guru'),
-                'password' => $this->request->getPost('password'),
-            );
-            //memindahkan file foto dari form input ke folder foto di directory
-            $foto->move('foto', $nama_file);
-            $this->Mdl_User->add($data);
-            session()->setFlashdata('pesan', 'Data berhasil di tambahkan');
+            $foto_guru = $this->request->getFile('foto_guru');
+            if ($foto_guru->getError() == 4) {
+                $data = array(
+                    'id_guru' => $id_guru,
+                    'kode_guru' => $this->request->getPost('kode_guru'),
+                    'nip' => $this->request->getPost('nip'),
+                    'nama_guru' => $this->request->getPost('nama_guru'),
+                    'password' => $this->request->getPost('password'),
+                    'foto_guru' => $this->request->getPost('foto_guru'),
+                );
+                $this->Mdl_Guru->edit($data);
+            } else {
+                //menghapus foto lama
+                $guru = $this->Mdl_Guru->detailData($id_guru);
+                if ($guru['foto_guru'] != "") {
+                    unlink('fotoguru/'. $foto_guru['foto_guru']);
+                }
+                //merename nama file foto
+                $nama_file = $foto_guru->getRandomName();
+                //jika valid
+                $data = array(
+                    'id_guru' => $id_guru,
+                    'kode_guru' => $this->request->getPost('kode_guru'),
+                    'nip' => $this->request->getPost('nip'),
+                    'nama_guru' => $this->request->getPost('nama_guru'),
+                    'password' => $this->request->getPost('password'),
+                    'foto_guru' => $nama_file,
+                );
+                //memindahkan file foto dari form input ke folder foto di directory
+                $foto_guru->move('foto_guru', $nama_file);
+                $this->Mdl_Guru->edit($data);
+            }
+            
+            session()->setFlashdata('pesan', 'Data berhasil di Ganti');
             return redirect()->to(base_url('guru'));
         } else {
             //jika tidak valid 
@@ -170,7 +190,20 @@ class Guru extends BaseController
         }
     }
 
-    
+    public function delete($id_guru)
+    {
+        //menghapus foto lama
+        $guru = $this->Mdl_Guru->detailData($id_guru);
+        if ($guru['foto_guru'] != "") {
+            unlink('fotoguru/'. $guru['foto_guru']);
+        }
+        $data = [
+            'id_guru' => $id_guru,
+        ];
+        $this->Mdl_Guru->delete_data($data);
+        session()->setFlashdata('pesan','Data berhasil dihapus!');
+        return redirect()->to(base_url('guru'));
+    }
 }
 
 
