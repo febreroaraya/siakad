2588c31aba01ecf7386844fe6be51fbe8da0840d
ahmadfadhiel <55206005+ahmadfadhiel@users.noreply.github.com>
2021-12-28 02:11:20 +0700

    db

---
+++ db_siakad (4).sql
@@ -0,0 +1,347 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 27, 2021 at 08:06 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_siakad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_guru`
+--
+
+CREATE TABLE `tbl_guru` (
+  `id_guru` int(11) NOT NULL,
+  `kode_guru` varchar(10) DEFAULT NULL,
+  `nip` varchar(18) DEFAULT NULL,
+  `nama_guru` varchar(40) DEFAULT NULL,
+  `foto_guru` varchar(255) DEFAULT NULL,
+  `password` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_guru`
+--
+
+INSERT INTO `tbl_guru` (`id_guru`, `kode_guru`, `nip`, `nama_guru`, `foto_guru`, `password`) VALUES
+(1, 'G000001', '111111111', 'Drs. Rahmat Basuki S.Pd ', 'guru1.png', 1234),
+(2, 'G00002', '111111112', 'Yuswa Leksmana S.Pd', 'guru2.png', 1234),
+(8, 'G000003', '111111113', 'Puguh Wicaksono S.Pd', 'guru3.jpg', 12345),
+(12, '123456789', '1122334455', 'Erni Fitria S ', '1640619268_6962f76583a4579f226a.jpg', 1234);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ruangan`
+--
+
+CREATE TABLE `tbl_ruangan` (
+  `id_ruangan` int(2) NOT NULL,
+  `id_gedung` int(2) DEFAULT NULL,
+  `ruangan` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_ruangan`
+--
+
+INSERT INTO `tbl_ruangan` (`id_ruangan`, `id_gedung`, `ruangan`) VALUES
+(3, 1, 'A3'),
+(4, 1, 'A4'),
+(5, 2, 'A5');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_siswa`
+--
+
+CREATE TABLE `tbl_siswa` (
+  `id_siswa` int(11) NOT NULL,
+  `nis` varchar(10) DEFAULT NULL,
+  `nama_siswa` varchar(50) DEFAULT NULL,
+  `id_jurusan` int(2) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `foto_siswa` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_siswa`
+--
+
+INSERT INTO `tbl_siswa` (`id_siswa`, `nis`, `nama_siswa`, `id_jurusan`, `password`, `foto_siswa`) VALUES
+(1, '1809599001', 'Tomo', 1, '12345', 'foto1.png'),
+(2, '1809599002', 'Udin', 2, '12345', 'foto1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_gedung`
+--
+
+CREATE TABLE `tb_gedung` (
+  `id_gedung` int(2) NOT NULL,
+  `gedung` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_gedung`
+--
+
+INSERT INTO `tb_gedung` (`id_gedung`, `gedung`) VALUES
+(1, 'Gedung A'),
+(2, 'Gedung B');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jurusan`
+--
+
+CREATE TABLE `tb_jurusan` (
+  `id_jurusan` int(2) NOT NULL,
+  `id_kelas` int(2) DEFAULT NULL,
+  `kode_jurusan` varchar(10) DEFAULT NULL,
+  `jurusan` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_jurusan`
+--
+
+INSERT INTO `tb_jurusan` (`id_jurusan`, `id_kelas`, `kode_jurusan`, `jurusan`) VALUES
+(1, 1, 'IPA', 'ilmu pengetahuan alam'),
+(2, 2, 'IPS', 'ilmu pengetahua sosial'),
+(6, 3, 'BI', 'Bahasa Indonesia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kelas`
+--
+
+CREATE TABLE `tb_kelas` (
+  `id_kelas` int(2) NOT NULL,
+  `kelas` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_kelas`
+--
+
+INSERT INTO `tb_kelas` (`id_kelas`, `kelas`) VALUES
+(1, 'Kelas 12'),
+(2, 'Kelas 11'),
+(3, 'Kelas 10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mapel`
+--
+
+CREATE TABLE `tb_mapel` (
+  `id_mapel` int(11) NOT NULL,
+  `kode_mapel` varchar(225) DEFAULT NULL,
+  `mapel` varchar(10) DEFAULT NULL,
+  `kategori` varchar(10) DEFAULT NULL,
+  `semester` varchar(10) DEFAULT NULL,
+  `id_jurusan` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_mapel`
+--
+
+INSERT INTO `tb_mapel` (`id_mapel`, `kode_mapel`, `mapel`, `kategori`, `semester`, `id_jurusan`) VALUES
+(1, '0011', 'IPA', 'wajib', 'Ganjil', 2),
+(2, '0033', 'IPS', 'wajib', 'Genap', 1),
+(3, '0044', 'FISIKA', 'wajib', 'Ganjil', 2),
+(4, '0066', 'KIMIA', 'wajib', 'Genap', 2),
+(7, '001', 'Matematika', 'wajib', 'Ganjil', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_ta`
+--
+
+CREATE TABLE `tb_ta` (
+  `id_ta` int(4) NOT NULL,
+  `ta` varchar(10) DEFAULT NULL,
+  `semester` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_ta`
+--
+
+INSERT INTO `tb_ta` (`id_ta`, `ta`, `semester`) VALUES
+(1, '2021/2022', 'Ganjil'),
+(2, '2022/2023', 'Genap'),
+(3, '2023/2024', 'Ganjil'),
+(4, '2024/2025', 'Ganjil'),
+(6, '2021/2022', 'Genap'),
+(7, '2022/2023', 'Ganjil'),
+(8, '2023/2024', 'Genap'),
+(9, '2024/2025', 'Genap');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(25) DEFAULT NULL,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nama_user`, `username`, `password`, `foto`) VALUES
+(1, 'Febrero', 'admin', 'admin', '1640593152_cb1ee272a471dae17999.png'),
+(10, 'Irfan', 'irfan', '12345', '1640612283_50f541371d95603b5ab3.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_guru`
+--
+ALTER TABLE `tbl_guru`
+  ADD PRIMARY KEY (`id_guru`);
+
+--
+-- Indexes for table `tbl_ruangan`
+--
+ALTER TABLE `tbl_ruangan`
+  ADD PRIMARY KEY (`id_ruangan`);
+
+--
+-- Indexes for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  ADD PRIMARY KEY (`id_siswa`);
+
+--
+-- Indexes for table `tb_gedung`
+--
+ALTER TABLE `tb_gedung`
+  ADD PRIMARY KEY (`id_gedung`);
+
+--
+-- Indexes for table `tb_jurusan`
+--
+ALTER TABLE `tb_jurusan`
+  ADD PRIMARY KEY (`id_jurusan`);
+
+--
+-- Indexes for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `tb_mapel`
+--
+ALTER TABLE `tb_mapel`
+  ADD PRIMARY KEY (`id_mapel`);
+
+--
+-- Indexes for table `tb_ta`
+--
+ALTER TABLE `tb_ta`
+  ADD PRIMARY KEY (`id_ta`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_guru`
+--
+ALTER TABLE `tbl_guru`
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tbl_ruangan`
+--
+ALTER TABLE `tbl_ruangan`
+  MODIFY `id_ruangan` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_gedung`
+--
+ALTER TABLE `tb_gedung`
+  MODIFY `id_gedung` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_jurusan`
+--
+ALTER TABLE `tb_jurusan`
+  MODIFY `id_jurusan` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  MODIFY `id_kelas` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tb_mapel`
+--
+ALTER TABLE `tb_mapel`
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tb_ta`
+--
+ALTER TABLE `tb_ta`
+  MODIFY `id_ta` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
